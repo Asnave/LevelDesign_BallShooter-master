5f6ecaa3e68739645ea3b16814c1806bc3f505a3
Abigail Evans <90458573+Asnave@users.noreply.github.com>
2021-10-01 15:27:30 -0300

    Lvl2.1

---
+++ Assets/transformer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transformer : MonoBehaviour
+
+{
+    [SerializeField] private List<Transform> waypoints;
+    [SerializeField] private float movespeed = 5f;
+    private int _currentWaypoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (waypoints.Count <= 0) return;
+        _currentWaypoint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+
+
+    private void HandleMovement()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[_currentWaypoint].transform.position, (movespeed * Time.deltaTime));
+
+        if
+
+    }
+
+}
